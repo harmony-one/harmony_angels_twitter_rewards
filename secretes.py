7d4e9c7a400d0cc9111f8409f13905cdeab79543
harmony-community-node <68701699+harmony-community-node@users.noreply.github.com>
2020-09-07 18:38:55 -0400

    Create secretes.py
    Added Secretes class

---
+++ secretes.py
@@ -0,0 +1,9 @@
+class Secretes:
+    twitterConsumerApiKey = "XXXXX"
+    twitterConsumerApiSecret = "XXXXX"
+    twitterAccessToken = "XXXX"
+    twitterAccessTokenSecret = "XXXX"
+    twitterBearerToken = "XXXXX"
+    rapidAPIKey = "XXXXX"
+    follower_wonk_access_id_str = "XXXXXX"
+    follower_wonk_secret_key_str = "XXXXXXXX"
