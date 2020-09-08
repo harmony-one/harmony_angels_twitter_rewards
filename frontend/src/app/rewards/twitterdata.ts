33bdf2e298ceef7e356372e54e92109a5140c14d
harmony-community-node <68701699+harmony-community-node@users.noreply.github.com>
2020-09-07 21:33:41 -0400

    Create twitterdata.ts
    creating front-end

---
+++ frontend/src/app/rewards/twitterdata.ts
@@ -0,0 +1,8 @@
+export class TweetData {
+    angle_twitter_handle: string;
+    angle_twitter_id: string;
+    bot_score: number;
+    reward: number;
+    sa_score: number;
+    tweet_id: string;
+}
