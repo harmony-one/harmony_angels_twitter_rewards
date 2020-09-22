8692bdfa56baa7bd63dca453b80d8a022f69002f
harmony-community-node <68701699+harmony-community-node@users.noreply.github.com>
2020-09-21 21:07:24 -0400

    Update reward.service.ts
    added method to get pow tweet details

--- frontend/src/app/rewards/reward.service.ts
+++ frontend/src/app/rewards/reward.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { TweetData } from './twitterdata'
+import { PowTweetInfo } from './pow_tweet_info'
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RewardService {
 
-  constructor(private firestore: AngularFirestore ) { }
+  constructor(private firestore: AngularFirestore,private http: HttpClient) { }
 
   getRewardsDetails(tweetId: string): Observable<TweetData[]> { 
     const data$ = this.firestore.collection("tweetData", ref => ref.where('tweet_id', '==', tweetId));
@@ -23,4 +25,19 @@ export class RewardService {
       });
     }));
  }
+
+  getTweetDate(tweetId) : Observable<any> 
+  {
+    const data$ = this.firestore.collection("pow_tweet_info", ref => ref.where('tweet_id', '==', tweetId));
+    return data$.snapshotChanges()
+    .pipe(map(snaps =>  {
+      return snaps.map(snap => {
+        return <PowTweetInfo>  {
+          id:snap.payload.doc.id,
+          ...snap.payload.doc.data() as PowTweetInfo
+        };
+      });
+    }));
+  }
+
 }
